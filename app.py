44066f05b9e8fa66fd45304ab6d7e27a2faf9e03
Jonathan Owen <jonathanjowen@gmail.com>
2016-06-05 16:46:25 -0400

    Deployment test with AAPL

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import numpy as np
+import pandas as pd
+import requests
+import simplejson as json
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure, show, output_file
+from bokeh.io import output_notebook
+from bokeh.embed import components 
 
 app = Flask(__name__)
 
@@ -8,7 +15,29 @@ def main():
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    stock = 'AAPL'
+
+    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
+    session = requests.Session()
+    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+    raw_data = session.get(api_url)
+    session.close()
+
+    stock_json = raw_data.json()
+    stock_df = pd.DataFrame(stock_json['data'], columns=stock_json['column_names'])
+    stock_df.set_index(pd.to_datetime(stock_df["Date"]), inplace=True)
+
+#    output_notebook()
+    p = figure(tools='pan,box_zoom,reset,save', title='ticker symbol: %s' % stock,
+           x_axis_label='date', x_axis_type='datetime')
+    x = stock_df.index
+    y = stock_df['Close']
+    p.line(x, y, line_color = 'blue')
+#    show(p)
+
+    script, div = components(p)
+    return render_template('index.html', script=script, div=div)
 
 if __name__ == '__main__':
     app.run(port=33507)
+
